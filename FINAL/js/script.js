f0af7861be758c33e2ab9b0c15425379d915927c
GunaySaffar <gunayava4776@gmail.com>
2017-08-07 05:45:49 +0400

    slider

--- FINAL/js/script.js
+++ FINAL/js/script.js
@@ -1,8 +1,30 @@
-$(document).ready(function(){
-  $('.slider2').bxSlider({
-    slideWidth: 300,
-    minSlides: 2,
-    maxSlides: 2,
-    slideMargin: 10
-  });
-});
+var slid=document.querySelector('.polosa');
+var btn=document.querySelectorAll('.btns');
+
+var count=0;
+ function slidermoveLeft() {
+ slid.style.left="-475px";
+ btn[0].style.background = "black";
+ btn[1].style.background="none";
+
+ }
+  function slidermoveRight() {
+  slid.style.left="0px";
+  btn[0].style.background = "none";
+  btn[1].style.background="black";
+ }
+ function autoMove(){
+  if (count==0) {
+  slid.style.left="-475px";
+  btn[0].style.background = "black";
+  btn[1].style.background="none";
+  count=1;
+  }
+  else {
+  slid.style.left="0px";
+  btn[0].style.background = "none";
+  btn[1].style.background="black";
+  count=0;
+  }
+ }
+ setInterval(autoMove,2000)
